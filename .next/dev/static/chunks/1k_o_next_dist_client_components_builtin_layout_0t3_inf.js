c43e3a7dc20d9de8835d926856afe760acc089d3
Fauziyah Martha Aula <FauziyahMarthaAula@saintek.unipdu.ac.id>
2026-06-19 21:38:28 +0700

    bisa

--- .next/dev/static/chunks/1k_o_next_dist_client_components_builtin_layout_0t3_inf.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/1k_o_next_dist_1xm5nnk._.js"],
-    source: "dynamic"
-});
